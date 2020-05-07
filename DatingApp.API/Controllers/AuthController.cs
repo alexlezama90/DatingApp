be994203e809e6ce13d66e3928199b959c205712
alexlezama90 <alexlezama90@hotmail.com>
2020-05-06 22:29:31 -0500

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -72,9 +72,11 @@ namespace DatingApp.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok( new {
+            return Ok(new
+            {
                 token = tokenHandler.WriteToken(token)
             });
+
         }
 
     }
